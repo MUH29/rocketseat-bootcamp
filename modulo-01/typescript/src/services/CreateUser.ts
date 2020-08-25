c8b12b4275114763ba697e85182088ef432de2ff
Adelmo Dias <adelmodias@outlook.com>
2020-08-25 20:50:30 -0300

    Modulo 1 - Typescript - Concluido

---
+++ modulo-01/typescript/src/services/CreateUser.ts
@@ -0,0 +1,31 @@
+/**
+ * Para criar: name, email, password
+ */
+
+interface techsObjects {
+    title: string;
+    experience: number;
+}
+
+interface CreateUserData {
+    name?: string;
+    email: string;
+    password: string;
+    techs: Array<string | techsObjects>;
+}
+
+export default function createUser({
+    name,
+    email,
+    password,
+    techs,
+}: CreateUserData) {
+    const user = {
+        name,
+        email,
+        password,
+        techs,
+    };
+
+    return user;
+}
