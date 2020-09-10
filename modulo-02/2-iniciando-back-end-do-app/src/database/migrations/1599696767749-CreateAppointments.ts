506a458dfccb4b2aa3601ac5ca44f4c26602e4e0
Adelmo Dias <adelmodias@outlook.com>
2020-09-09 22:45:29 -0300

    Repositório do TypeORM

--- modulo-02/2-iniciando-back-end-do-app/src/database/migrations/1599696767749-CreateAppointments.ts
+++ modulo-02/2-iniciando-back-end-do-app/src/database/migrations/1599696767749-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1599696767749
                         type: "varchar",
                         isPrimary: true,
                         generationStrategy: "uuid",
+                        default: "uuid_generate_v4()",
                     },
                     {
                         name: "provider",
